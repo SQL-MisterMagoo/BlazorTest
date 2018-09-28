c2fb10c2a4f46361470d453bf98ca11dd7aa1335
Mister Magoo <mister.magoo+githubsql@gmail.com>
2018-09-28 16:07:52 +0100

    Trying to moev components to their own projects - but hit a stumbling block that might be fixed in 0.6.0-preview1-final

---
+++ BlazorClock/Model/TickerModel.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.AspNetCore.Blazor.Components;
+using System;
+using System.Threading.Tasks;
+
+namespace BlazorClock.Model
+{
+	public class TickerModel : BlazorComponent
+	{
+		[Parameter] internal string Title { get; set; }
+		[Parameter] internal double OffsetHours { get; set; }
+		[Parameter] internal double OffsetMinutes { get; set; }
+		[Parameter] internal string TimeZoneId { get; set; }
+		[Parameter] internal bool AutoTitle { get; set; }
+		[Parameter] internal bool ShowIcon { get; set; }
+		[Parameter] internal double Width { get; set; }
+		[Parameter] internal double Height { get; set; }
+
+		internal string Data;
+		internal double hourRotation;
+		internal double minuteRotation;
+		internal double secondRotation;
+
+		private Task ticker;
+		private TimeZoneInfo timeZone;
+
+		protected override void OnInit()
+		{
+			if (!string.IsNullOrWhiteSpace(TimeZoneId))
+			{
+				try
+				{
+					timeZone = TimeZoneInfo.FindSystemTimeZoneById(TimeZoneId);
+				}
+				catch
+				{
+					timeZone = TimeZoneInfo.Local;
+				}
+			} else if (OffsetHours == 0 && OffsetMinutes == 0)
+			{
+				timeZone = TimeZoneInfo.Local;
+			}  else
+			{
+				timeZone = TimeZoneInfo.CreateCustomTimeZone(Title ?? "Clock", new TimeSpan((int)OffsetHours, (int)OffsetMinutes, 0), Title ?? "Clock", Title ?? "Clock");
+			}
+			if (Width == 0) Width = 600;
+			if (Height == 0) Height = 600;
+			ticker = RunTicker();
+		}
+
+		private async Task RunTicker()
+		{
+			while (true)
+			{
+				await Task.Delay(1000);
+				DateTime utcNow = DateTime.UtcNow;
+				if (AutoTitle) Title = timeZone.IsDaylightSavingTime(utcNow) ? timeZone.DaylightName : timeZone.StandardName;
+				DateTime dateTime = utcNow.Add(timeZone.GetUtcOffset(utcNow));
+				Data = dateTime.ToLongTimeString();
+				if (!ShowIcon) UpdateSvg(dateTime);
+				StateHasChanged();
+			}
+		}
+
+		private void UpdateSvg(DateTime date)
+		{
+			var hr = date.Hour;
+			var min = date.Minute;
+			var sec = date.Second;
+
+			hourRotation = (hr * 360 / 12) + (min * (360 / 60) / 12);
+			minuteRotation = (min * 360 / 60) + (sec * (360 / 60) / 60);
+			secondRotation = sec * 360 / 60;
+		}
+	}
+}
---
+++ BlazorFlying/Model/FlyingModel.cs
@@ -0,0 +1,98 @@
+﻿using Microsoft.AspNetCore.Blazor.Components;
+using System;
+using System.Threading.Tasks;
+
+namespace BlazorFlying.Model
+{
+	public class FlyingModel : BlazorComponent
+	{
+		[Parameter]
+		internal string Image { get; set; }
+		[Parameter]
+		internal bool Debug { get; set; }
+		[Parameter]
+		internal double X { get; set; }
+		[Parameter]
+		internal double Y { get; set; }
+		[Parameter]
+		internal double Speed { get; set; }
+		[Parameter]
+		internal double frameDelay { get; set; }
+
+		internal (double x, double y) Position;
+		internal (double x, double y) NewPosition;
+		internal int direction;
+		private Random random = new Random();
+		private long lastTime;		
+
+		private async Task<bool> StartAnimation()
+		{
+
+			lastTime = DateTime.UtcNow.Ticks / TimeSpan.TicksPerMillisecond;
+			while (true)
+			{
+				await Task.Delay((int)frameDelay);
+
+				if (TuplesAreClose(Position, NewPosition))
+				{
+					NewPosition = NewRandomPosition();
+					Console.WriteLine($"Aiming for {NewPosition.x:N2} , {NewPosition.y:N2}");
+				}
+
+				Update();
+
+			}
+		}
+
+		protected override void OnInit()
+		{
+			base.OnInit();
+			Position = (X, Y);
+			NewPosition = Position;
+			frameDelay = frameDelay == 0 ? 40 : frameDelay;
+			StartAnimation().ConfigureAwait(false);
+			return;
+		}
+
+		private (double x, double y) NewRandomPosition()
+		{
+			double rx = random.NextDouble() * 70;
+			double ry = random.NextDouble() * 70;
+			return (rx, ry);
+		}
+
+		private bool TuplesAreClose((double x, double y) A, (double x, double y) B)
+		{
+			bool close = Math.Abs(A.x - B.x) < 0.5 && Math.Abs(A.y - B.y) < 0.5;
+			return close;
+		}
+
+		private void Update()
+		{
+			long thisTime = DateTime.UtcNow.Ticks / TimeSpan.TicksPerMillisecond;
+			double dT = (double)(thisTime - lastTime) / frameDelay;
+			lastTime = thisTime;
+
+			double rx = NewPosition.x - Position.x;
+			if (Math.Abs(rx) < 0.5) rx = 0;
+			//double xFix = Math.Abs(rx) < 5 ? (1 / (5 - Math.Abs(rx))) : 1;
+			//double dx = Math.Max( Math.Min(Math.Sign(rx) * dT , xFix), Math.Sign(rx) * xFix);
+
+			double ry = NewPosition.y - Position.y;
+			if (Math.Abs(ry) < 0.5) ry = 0;
+			//double yFix = Math.Abs(ry) < 5 ? (1 / (5 - Math.Abs(ry))) : 1;
+			//double dy = Math.Max(Math.Min(Math.Sign(ry) * dT, yFix), Math.Sign(ry) * yFix);
+
+			double dx = Speed * dT * Math.Sign(rx) / (1000 / frameDelay);
+			double dy = Speed * dT * Math.Sign(ry) / (1000 / frameDelay);
+			if (Math.Abs(rx) < 5) dx /= 3;
+			if (Math.Abs(ry) < 5) dy /= 3;
+
+			Position = (Position.x + dx, Position.y + dy);
+			direction = rx < 0 ? -1 : 1;
+			StateHasChanged();
+
+		}
+
+	}
+}
