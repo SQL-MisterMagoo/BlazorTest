caeb255d7e4c2dc764e4254e12c82e3576135ab7
SQL-MisterMagoo <mister.magoo+githubsql@gmail.com>
2018-10-03 02:08:41 +0100

    Add Identity (#1)
    Added Identity with a slightly hacky login / logout from Blazor.App - it works but requires a page reload - maybe that can be avoided?

---
+++ BlazorTest.Server/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using BlazorTest.Server.Areas.Identity.Data;
+using BlazorTest.Server.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(BlazorTest.Server.Areas.Identity.IdentityHostingStartup))]
+namespace BlazorTest.Server.Areas.Identity
+{
+	public class IdentityHostingStartup : IHostingStartup
+	{
+		public void Configure(IWebHostBuilder builder)
+		{
+			builder.ConfigureServices((context, services) =>
+			{
+				services.AddDbContext<BlazorTestServerContext>(options =>
+						options.UseSqlite(
+								context.Configuration.GetConnectionString("BlazorTestServerContextConnection")));
+
+				services.AddDefaultIdentity<BlazorTestServerUser>()
+						.AddEntityFrameworkStores<BlazorTestServerContext>();
+			});
+		}
+	}
+}
--- BlazorTest.Server/Startup.cs
+++ BlazorTest.Server/Startup.cs
@@ -1,15 +1,29 @@
+using BlazorTest.Server.Areas.Identity.Data;
+using BlazorTest.Server.Models;
 using Microsoft.AspNetCore.Blazor.Server;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.ResponseCompression;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using System;
 using System.Linq;
 using System.Net.Mime;
+using Microsoft.AspNetCore.Mvc;
 
 namespace BlazorTest.Server
 {
 	public class Startup
 	{
+		public IConfiguration Configuration { get; }
+
+		public Startup(IConfiguration configuration)
+		{
+			Configuration = configuration;
+		}
 		// This method gets called by the runtime. Use this method to add services to the container.
 		// For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
 		public void ConfigureServices(IServiceCollection services)
@@ -25,6 +39,22 @@ namespace BlazorTest.Server
 										WasmMediaTypeNames.Application.Wasm,
 					});
 			});
+
+			services.Configure<CookiePolicyOptions>(options =>
+			{
+				// This lambda determines whether user consent for non-essential cookies is needed for a given request.
+				options.CheckConsentNeeded = context => true;
+				options.MinimumSameSitePolicy = SameSiteMode.None;
+			});
+			
+			services.AddAuthentication().AddMicrosoftAccount(microsoftOptions =>
+			{
+				microsoftOptions.ClientId = Configuration["Authentication:Microsoft:ApplicationId"];
+				microsoftOptions.ClientSecret = Configuration["Authentication:Microsoft:Password"];
+				microsoftOptions.CallbackPath = "/signin-microsoft";
+			});
+			services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
 		}
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -35,10 +65,28 @@ namespace BlazorTest.Server
 			if (env.IsDevelopment())
 			{
 				app.UseDeveloperExceptionPage();
+				app.UseDatabaseErrorPage();
+			}
+			else
+			{
+				app.UseExceptionHandler("/Home/Error");
+				app.UseHsts();
 			}
 
+			app.UseHttpsRedirection();
+			app.UseStaticFiles();
+			app.UseCookiePolicy();
+
+			app.UseAuthentication();
+
+			app.UseMvc(routes =>
+			{
+				routes.MapRoute(
+									name: "default",
+									template: "{controller=Home}/{action=Index}/{id?}");
+			});
 			// Use component registrations and static files from the app project.
-			app.UseServerSideBlazor<App.Startup>();
+			app.Map("/blz", (child) => child.UseServerSideBlazor<App.Startup>());
 		}
 	}
 }
