0947c8563e279771524223ee69dada2751e5e699
Mister Magoo <mister.magoo+githubsql@gmail.com>
2018-10-06 23:58:30 +0100

    Upgrade to 0.6.0 release Added GenFu for sample TreeView Added basic TreeView component

---
+++ BlazorTree/INodeItem.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorTree
+{
+	public interface INodeItem<T>
+	{
+		IEnumerable<T> Dependents { get; set; }
+		int Depth { get; set; }
+	}
+}
