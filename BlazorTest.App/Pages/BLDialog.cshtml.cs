250dc11c84a9559e542e1c3e9018da03a8320db2
Mister Magoo <mister.magoo+githubsql@gmail.com>
2018-10-16 11:54:19 +0100

    Added an annoying popup page because every modern website needs one. Renamed page(1|2)

---
+++ BlazorTest.App/Pages/BLDialog.cshtml.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Blazor;
+using Microsoft.AspNetCore.Blazor.Components;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace BlazorTest.App.Pages
+{
+	public class BLDialogModel : BlazorComponent
+	{
+		Timer timer;
+
+		public bool ShowDialog { get; private set; }
+
+		protected override void OnInit()
+		{
+			base.OnInit();
+			timer = new Timer(2000);
+			timer.Elapsed += AnnoyPeople;
+			timer.Start();
+		}
+
+		private void AnnoyPeople(object sender, ElapsedEventArgs e)
+		{
+			timer.Stop();
+			ShowDialog = true;
+			StateHasChanged();
+		}
+
+		internal void OnClick(UIMouseEventArgs args)
+		{
+			timer.Start();
+			ShowDialog = false;
+			StateHasChanged();
+		}
+	}
+}
