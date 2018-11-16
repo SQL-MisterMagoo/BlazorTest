39623ed6c0a7c74a478e2feb81bb74b459d66381
Mister Magoo <mister.magoo+githubsql@gmail.com>
2018-11-16 02:05:30 +0000

    Add CascadingValue to Tree page and BlazorTree component

---
+++ BlazorTree/ITreeSelection.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorTree
+{
+	/// <summary>
+	/// Interface ITreeSelection can be implemented by the consumer and made available through CascadeValue to the <see cref="BlazorTree">BlazorTree</see>
+	/// </summary>
+	/// <typeparam name="T">Generic Type that matches the Type of data passed to the tree to display.</typeparam>
+	public interface ITreeSelection<T>
+	{
+		/// <summary>
+		/// Implement this method so that the Tree nodes can determine if they have been selected.
+		/// </summary>
+		/// <param name="Item">The tree node will pass in an Item of Type T</param>
+		/// <returns></returns>
+		bool IsSelected(T Item);
+		/// <summary>
+		/// Returns a list of all selected Nodes.
+		/// </summary>
+		/// <returns>IEnumerable of T</returns>
+		IEnumerable<T> GetSelected();
+		/// <summary>
+		/// Implement this method to allow nodes to be selected.
+		/// </summary>
+		/// <param name="Item">The Item of Type T to select.</param>
+		/// <param name="ClearOtherSelections">Set true to make this the only selected node.</param>
+		void Select(T Item, bool ClearOtherSelections);
+		/// <summary>
+		/// Clear all selections.
+		/// </summary>
+		void Clear();
+	}
+}
---
+++ BlazorTree/TreeSelection.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace BlazorTree
+{
+	public class TreeSelection<T> : ITreeSelection<T>
+	{
+		List<T> SelectedItems;
+
+		public TreeSelection()
+		{
+			SelectedItems = new List<T>();
+		}
+
+		public void Clear()
+		{
+			SelectedItems.Clear();
+		}
+
+		public IEnumerable<T> GetSelected()
+		{
+			return SelectedItems?.AsEnumerable<T>();
+		}
+
+		public bool IsSelected(T Item)
+		{
+			return SelectedItems?.Contains(Item) ?? false;
+		}
+
+		public void Select(T Item, bool ClearOtherSelections = false)
+		{
+			if (IsSelected(Item))
+			{
+				SelectedItems.Remove(Item);
+				return;
+			}
+			if (ClearOtherSelections)
+			{
+				Clear();
+			}
+			SelectedItems?.Add(Item);
+		}
+	}
+}
