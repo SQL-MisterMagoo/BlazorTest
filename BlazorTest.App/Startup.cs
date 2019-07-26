91c0ee7baac377ea9df1e387e910d4166dfc27e1
Mister Magoo <mister.magoo+githubsql@gmail.com>
2019-07-26 17:39:15 +0100

    Seems to work in Preview7 now.

--- BlazorTest.App/Startup.cs
+++ BlazorTest.App/Startup.cs
@@ -1,5 +1,9 @@
+using BlazorSolidLogin.Services;
+using BlazorTest.Server.Areas.Identity.Data;
 using Microsoft.AspNetCore.Components.Builder;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.JSInterop;
 
 namespace BlazorTestApp
 {
@@ -7,13 +11,13 @@ namespace BlazorTestApp
     {
         public void ConfigureServices(IServiceCollection services)
         {
-            //services.AddScoped<UserManager<BlazorTestServerUser>, UserManager<BlazorTestServerUser>>();
-            //services.AddScoped<SignInManager<BlazorTestServerUser>, SignInManager<BlazorTestServerUser>>();
-            //services.AddTransient<ILoginNotifier>((a) =>
-            //{
-            //    var JSRuntime = a.GetRequiredService<IJSRuntime>();
-            //    return new SolidIdentityService(JSRuntime);
-            //});
+            services.AddScoped<UserManager<BlazorTestServerUser>, UserManager<BlazorTestServerUser>>();
+            services.AddScoped<SignInManager<BlazorTestServerUser>, SignInManager<BlazorTestServerUser>>();
+            services.AddTransient<ILoginNotifier>((a) =>
+            {
+                var JSRuntime = a.GetRequiredService<IJSRuntime>();
+                return new SolidIdentityService(JSRuntime);
+            });
 
             //services.ToList().ForEach(s => Console.WriteLine($"Service: {s.ServiceType.Name} - {s.Lifetime.ToString()}"));
 
